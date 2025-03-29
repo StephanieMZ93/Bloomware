c9afb91a32a0c3149b69531a7572e780d9a22ebb
Stephanie Martinez Zuluaga <105985136+StephanieMZ93@users.noreply.github.com>
2025-03-29 18:33:35 -0500

    Creación de Inicio y Cierre de Sesión

--- principal.php
+++ principal.php
@@ -4,7 +4,7 @@ session_start();
 // Verificar si el usuario ha iniciado sesión
 if (!isset($_SESSION["usuario_id"])) {
     // Si no ha iniciado sesión, redirigir al formulario de inicio de sesión
-    header("Location: index.php"); // Redirige al archivo index.php (o al que contenga el formulario de login)
+    header("Location: index.php");
     exit();
 }
 
@@ -17,45 +17,58 @@ if (!isset($_SESSION["usuario_id"])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Bloomware-interfaz principal</title>
-    <link rel="stylesheet" href="/css/IPrincipal.css" />
+    <link rel="stylesheet" href="css/IPrincipal.css" />
 </head>
 <body>
     <div class="container">
         <header class="header">
             <img src="img/logo.jpeg.JPG" alt="Bloomware Logo" class="logo--img" />
-            <button class="cerrar-sesion">
-                <a href="logout.php" style="color:white; text-decoration:none;">Cerrar Sesión</a>
-            </button>
+            <a href="logout.php" class="cerrar-sesion">Cerrar Sesión</a>
         </header>
 
         <div class="card-container">
             <div class="card">
-                <img src="/img/usuario.png" alt="Usuarios">
-                <div class="card-content">
-                    <a href="#" class="card-title">Usuarios</a>
-                </div>
+                <a href="#">
+                    <img src="img/usuario.png" alt="Usuarios">
+                    <div class="card-content">
+                        <h2 class="card-title">Usuarios</h2>
+                    </div>
+                </a>
             </div>
             <div class="card">
-                <img src="/img/Proveedores.png" alt="Proveedores">
-                <div class="card-content">
-                    <a href="#" class="card-title">Proveedores</a>
-                </div>
+                <a href="#">
+                    <img src="img/Proveedores.png" alt="Proveedores">
+                    <div class="card-content">
+                        <h2 class="card-title">Proveedores</h2>
+                    </div>
+                </a>
             </div>
             <div class="card">
-                <img src="/img/Productos.png" alt="Productos">
-                <div class="card-content">
-                    <a href="#" class="card-title">Productos</a>
-                </div>
+                <a href="#">
+                    <img src="img/Productos.png" alt="Productos">
+                    <div class="card-content">
+                        <h2 class="card-title">Productos</h2>
+                    </div>
+                </a>
             </div>
             <div class="card">
-                <img src="/img/Clientes.png" alt="Clientes">
-                <div class="card-content">
-                    <a href="#" class="card-title">Clientes</a>
-                </div>
+                <a href="#">
+                    <img src="img/Clientes.png" alt="Clientes">
+                    <div class="card-content">
+                        <h2 class="card-title">Clientes</h2>
+                    </div>
+                </a>
             </div>
+            <div class="card">
+                <a href="#">
+                    <img src="img/Reportes.png" alt="Reportes">
+                    <div class="card-content">
+                        <h2 class="card-title">Reportes</h2>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
-    <script src="/JS/principal.js"></script>
+    <script src="JS/principal.js"></script>
 </body>
 </html>
