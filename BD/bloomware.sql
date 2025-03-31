a6258ba0577ddda5227f28649a3d895eb1d8c4e6
Marce05quiroga <jenny_mquiroga@soy.sena.edu.co>
2025-03-30 20:38:59 -0500

    Actualización de BD

--- BD/bloomware.sql
+++ BD/bloomware.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 29-03-2025 a las 23:49:24
--- Versión del servidor: 8.4.3
+-- Tiempo de generación: 31-03-2025 a las 01:36:31
+-- Versión del servidor: 9.1.0
 -- Versión de PHP: 8.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -30,11 +30,11 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `administrador`;
 CREATE TABLE IF NOT EXISTS `administrador` (
   `ID_Administrador` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Apellidos` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Email` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Telefono` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Contraseña` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Apellidos` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Telefono` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Contraseña` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`ID_Administrador`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
@@ -55,11 +55,11 @@ INSERT INTO `administrador` (`ID_Administrador`, `Nombre`, `Apellidos`, `Email`,
 DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE IF NOT EXISTS `cliente` (
   `ID_Cliente` int NOT NULL,
-  `ID_Producto` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Nombre` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Apellidos` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Email` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Telefono` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `ID_Producto` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Apellidos` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Telefono` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`ID_Cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
@@ -72,26 +72,13 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 DROP TABLE IF EXISTS `empleado`;
 CREATE TABLE IF NOT EXISTS `empleado` (
   `ID_Empleado` int NOT NULL,
-  `Nombre` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Apellidos` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Email` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Telefono` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Cargo` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Contraseña` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Apellidos` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Telefono` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Cargo` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Contraseña` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`ID_Empleado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `gestionar`
---
-
-DROP TABLE IF EXISTS `gestionar`;
-CREATE TABLE IF NOT EXISTS `gestionar` (
-  `Producto_ID_Producto` int NOT NULL,
-  `Cliente_ID_Cliente` int NOT NULL,
-  PRIMARY KEY (`Producto_ID_Producto`,`Cliente_ID_Cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 -- --------------------------------------------------------
@@ -105,13 +92,31 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `ID_Producto` int NOT NULL AUTO_INCREMENT,
   `Nombre_Producto` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
   `Cantidad` int NOT NULL,
-  `Precio` int NOT NULL,
-  `Categoria` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Lote` int NOT NULL,
+  `precio` decimal(10,0) DEFAULT NULL,
+  `Categoria` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Lote` varchar(10) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
   `Fecha_Vencimiento` date NOT NULL,
   `Administrador_ID_Administrador` int NOT NULL,
   PRIMARY KEY (`ID_Producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`ID_Producto`, `Nombre_Producto`, `Cantidad`, `precio`, `Categoria`, `Lote`, `Fecha_Vencimiento`, `Administrador_ID_Administrador`) VALUES
+(1, 'Brillo_Essence_Juicy_Bomb_Shiny', 10, 8500, 'brillo', '20250115A', '2025-05-11', 1),
+(2, 'Pestañina_Vogue_Curvas_Perfectas', 9, 24950, 'pestañinas', '20241025B', '0000-00-00', 2),
+(3, 'Polvo_Suelto_Samy_Translucido_Matte_Touch', 8, 19950, 'polvo', '20240926C', '2024-09-26', 2),
+(4, 'Labial_Ruby_Rose_Lip_Oil_Sandía', 4, 13560, 'brillo', '20268967B', '2025-05-13', 2),
+(5, 'Pestañina_Maybelline_Sky_High_Black_Waterproo', 7, 64950, 'pestañinas', '20267936A', '2024-11-23', 2),
+(6, 'Polvo_Compacto_Ana_María_Spf_25_Avellana', 15, 27950, 'pestañinas', '20368946N', '2024-12-08', 2),
+(7, 'Rubor_Barra_Essence_Baby_Got_Blush_Tn10', 6, 16500, 'Maquillaje_Facial', '21348046F', '2025-03-08', 2),
+(8, 'Lapiz_Essence_Designer_Brown_Cejas', 1, 10950, 'Lapiz', '26789059F', '2024-08-08', 2),
+(9, 'Polvo_Compacto_Aclarante_Ana_María_Spf_25_Ave', 15, 32950, 'polvo compacto', '23649047F', '2024-12-05', 2),
+(10, 'Labial_Vogue_Líquido_Colorissimo_Extra_Brillo', 5, 14950, 'labiales_liquidos', '20316798B', '2024-05-29', 2),
+(11, 'Brillo_Maybelline_Lip_Lifter_Gloss_Petal', 5, 60950, 'brillo', '20316798B', '2024-01-29', 2),
+(12, 'BrPolvo_Compacto_Maybelline_Super_Natural_Bei', 12, 35950, 'polvos', '20786708C', '2024-04-29', 2);
 
 -- --------------------------------------------------------
 
@@ -122,26 +127,12 @@ CREATE TABLE IF NOT EXISTS `producto` (
 DROP TABLE IF EXISTS `proveedores`;
 CREATE TABLE IF NOT EXISTS `proveedores` (
   `Nit_Proveedor` int NOT NULL,
-  `Nombre` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Direccion` varchar(60) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Telefono` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Categoria` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Email` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Direccion` varchar(60) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Telefono` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Categoria` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`Nit_Proveedor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `realizacion`
---
-
-DROP TABLE IF EXISTS `realizacion`;
-CREATE TABLE IF NOT EXISTS `realizacion` (
-  `Reporte_ID_Reporte` int NOT NULL,
-  `Empleado_ID_Empleado` int NOT NULL,
-  `Fecha` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  PRIMARY KEY (`Reporte_ID_Reporte`,`Empleado_ID_Empleado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 -- --------------------------------------------------------
@@ -154,25 +145,12 @@ DROP TABLE IF EXISTS `reporte`;
 CREATE TABLE IF NOT EXISTS `reporte` (
   `ID_Reporte` int NOT NULL AUTO_INCREMENT,
   `Fecha y hora` date NOT NULL,
-  `ID_Clientes` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `ID_Productos` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `ID_Usuario` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Nit_Proveedor` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `ID_Clientes` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `ID_Productos` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `ID_Usuario` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Nit_Proveedor` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `Administrador_ID_Administrador` int NOT NULL,
   PRIMARY KEY (`ID_Reporte`,`Administrador_ID_Administrador`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `suministra`
---
-
-DROP TABLE IF EXISTS `suministra`;
-CREATE TABLE IF NOT EXISTS `suministra` (
-  `Producto_ID_Producto` int NOT NULL,
-  `Proveedores_Nit_Proveedor` int NOT NULL,
-  PRIMARY KEY (`Producto_ID_Producto`,`Proveedores_Nit_Proveedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 -- --------------------------------------------------------
@@ -184,11 +162,11 @@ CREATE TABLE IF NOT EXISTS `suministra` (
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `ID_Usuario` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Apellido` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Email` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Telefono` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `Contraseña` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Apellido` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Telefono` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Contraseña` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `Administrador_ID_Administrador` int NOT NULL,
   PRIMARY KEY (`ID_Usuario`,`Administrador_ID_Administrador`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
@@ -201,19 +179,6 @@ INSERT INTO `usuario` (`ID_Usuario`, `Nombre`, `Apellido`, `Email`, `Telefono`, 
 (1, 'Stephanie', 'Martinez', 'smz021093@gmail.com', '3116250040', 'Mazu93.', 1),
 (2, 'Marcela', 'Quiroga', 'marceq0725@gmail.com', '3507740524', 'Marce123', 2),
 (3, 'Vendedor', '1', 'vendedor1@gmail.com', '3205428756', 'Vendedor1.', 0);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `vende`
---
-
-DROP TABLE IF EXISTS `vende`;
-CREATE TABLE IF NOT EXISTS `vende` (
-  `Empleado_ID_Empleado` int NOT NULL,
-  `Producto_ID_Producto` int NOT NULL,
-  PRIMARY KEY (`Empleado_ID_Empleado`,`Producto_ID_Producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
